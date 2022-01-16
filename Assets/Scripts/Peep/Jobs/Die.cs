081eac8ec06024760278c14e1868fd9b0e41d265
RealLuckyToast <felix.haensel@stud.hs-kempten.de>
2022-01-16 15:37:55 +0100

    added animation transition
    added transition for falling in lava and changed the animation for falling in lava and water

--- Assets/Scripts/Peep/Jobs/Die.cs
+++ Assets/Scripts/Peep/Jobs/Die.cs
@@ -6,7 +6,19 @@ public class Die : MonoBehaviour
 {
     private void Start()
     {
+        //Set Animation Bool to true
+        gameObject.GetComponent<Animator>().SetBool("lava", true);
+
+
+        StartCoroutine(Animation());
+        
+    }
+    private IEnumerator Animation()
+    {
+
+        //wait for the animation to finish
+        gameObject.GetComponent<PeepMovement>().enabled = false;
+        yield return new WaitForSecondsRealtime(2f);
         gameObject.GetComponent<Peep>().die();
     }
-
 }
